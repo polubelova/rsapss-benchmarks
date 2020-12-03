a22487972bd261539e1cf4f807c0fd8fd4d5fce1
Marina Polubelova <polubelovam@gmail.com>
2020-12-03 23:05:29 +0100

    add comparison with BN_mod_exp_mont_consttime

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@ OPENSSL_HOME ?= ../openssl
 
 TARGETS = $(patsubst %.c,%.exe,$(wildcard *.c))
 CFLAGS := -I$(KREMLIN_HOME)/include -I$(HACL_HOME)/dist/gcc-compatible \
+	-I$(HACL_HOME)/code/bignum/dist \
 	-I$(KREMLIN_HOME)/kremlib/dist/minimal \
 	-I$(BORINGSSL_HOME)/include -I$(BORINGSSL_HOME)/include/openssl \
 	-I$(OPENSSL_HOME)/include -I$(OPENSSL_HOME)/include/crypto -I$(OPENSSL_HOME)/crypto \
@@ -23,6 +24,11 @@ librsapss.a:
 	OTHERFLAGS="--admit_smt_queries true" make -C $(HACL_HOME)/code/rsapss dist/librsapss.a && \
 	cp $(HACL_HOME)/code/rsapss/dist/librsapss.a librsapss.a
 
+libbignum.a:
+	rm -rf $(HACL_HOME)/code/bignum/dist/*.a $(HACL_HOME)/code/bignum/dist/*.o && \
+	OTHERFLAGS="--admit_smt_queries true" make -C $(HACL_HOME)/code/bignum dist/libbignum.a && \
+	cp $(HACL_HOME)/code/bignum/dist/libbignum.a libbignum.a
+
 rsapss-openssl-test.exe: librsapss.a rsapss-openssl-test.o
 	$(CC) $(CFLAGS) $(LDFLAGS) $^ librsapss.a -o $@ $(OPENSSL_HOME)/libcrypto.a -lpthread -ldl
 
@@ -31,6 +37,9 @@ rsapss-openssl-test-4096.exe: librsapss.a rsapss-openssl-test-4096.o
 
 rsapss-boringssl-test.exe: librsapss.a rsapss-boringssl-test.o
 	$(CC) $(CFLAGS) $(LDFLAGS) $^ librsapss.a -o $@ $(BORINGSSL_HOME)/build/crypto/libcrypto.a -lpthread -ldl
+
+bignum-openssl-test.exe: libbignum.a bignum-openssl-test.o
+	$(CC) $(CFLAGS) $(LDFLAGS) $^ libbignum.a -o $@ $(OPENSSL_HOME)/libcrypto.a -lpthread -ldl
 
 %.exe: %.o
 	$(CC) $(CFLAGS) $(LDFLAGS) $^ librsapss.a -o $@
---
+++ bignum-openssl-test.c
@@ -0,0 +1,167 @@
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <time.h>
+
+#include "Hacl_Bignum64.h"
+#include "test_helpers.h"
+
+#include "openssl/crypto.h"
+#include "openssl/err.h"
+#include "openssl/rsa.h"
+#include "openssl/bn.h"
+
+#define ROUNDS 1000
+#define SIZE   1
+
+
+void print_bytes(uint32_t len, uint8_t *in){
+  for (int i = 0; i < len; i++)
+    printf("%02x, ", in[i]);
+  printf("\n");
+}
+
+void ossl_print(const BIGNUM* n)
+{
+  int num = (BN_num_bits(n)+7)/8;
+  uint8_t to[num];
+  BN_bn2binpad(n, to, num);
+  print_bytes(num, to);
+}
+
+void hacl_print(uint32_t len, uint64_t *b)
+{
+  uint8_t to[len];
+  Hacl_Bignum64_bn_to_bytes_be(len, b, to);
+  print_bytes(len, to);
+}
+
+void ossl_bn_mod_exp_ct(BIGNUM *a, BIGNUM *b, BIGNUM *n, BIGNUM *c, BN_CTX *ctx){
+  // c == a ^^ b % n
+  BN_mod_exp_mont_consttime(c, a, b, n, ctx, NULL);
+  /* ossl_print(a); */
+  /* ossl_print(b); */
+  /* ossl_print(c); */
+  /* ossl_print(n); */
+}
+
+void hacl_bn_mod_exp_ct(uint32_t len, uint64_t *n, uint64_t *a, uint32_t bBits, uint64_t *b, uint32_t l, uint64_t *r2, uint64_t *res){
+  Hacl_Bignum64_bn_mod_exp_fw_precompr2_ct(len, n, a, bBits, b, l, r2, res);
+  /* hacl_print(8*len, a); */
+  /* hacl_print(8*len, b); */
+  /* hacl_print(8*len, res); */
+  /* hacl_print(8*len, n); */
+}
+
+uint64_t *bn_ossl_to_hacl(uint32_t nBytes, BIGNUM *a){
+  uint8_t ab[nBytes];
+  BN_bn2binpad(a, ab, nBytes);
+  return Hacl_Bignum64_new_bn_from_bytes_be(nBytes, ab);
+}
+
+void bn_compare_and_print(uint32_t nBytes, BIGNUM *a, uint64_t *b){
+  uint8_t ab[nBytes];
+  BN_bn2binpad(a, ab, nBytes);
+
+  uint8_t bb[nBytes];
+  Hacl_Bignum64_bn_to_bytes_be(nBytes, b, bb);
+  compare_and_print(nBytes, ab, bb);
+}
+
+
+void test(int nBits){
+  cycles c1,c2;
+  clock_t t1,t2;
+
+  BN_CTX *ctx = BN_CTX_new();
+  BIGNUM *a = NULL, *b = NULL, *n = NULL, *c = NULL;
+  a = BN_new();
+  b = BN_new();
+  n = BN_new();
+  c = BN_new();
+  //BN_MONT_CTX *mont = NULL;
+  //mont = BN_MONT_CTX_new();
+  //BN_MONT_CTX_set(mont, n, ctx));
+
+  int nBytes = (nBits - 1) / 8 + 1;
+  int nLen = (nBits - 1) / 64 + 1;
+
+  BN_rand(n, nBits, 1, 1);
+  BN_rand_range(a, n);
+  BN_rand(b, nBits, 1, 0);
+
+  uint64_t *ah = bn_ossl_to_hacl(nBytes, a);
+  uint64_t *bh = bn_ossl_to_hacl(nBytes, b);
+  uint64_t *nh = bn_ossl_to_hacl(nBytes, n);
+  uint64_t ch[nLen];
+  uint64_t *r2 = Hacl_Bignum64_new_precompr2(nLen, nh);
+
+  //ossl_bn_mod_exp_ct(a, b, n, c, ctx);
+  //hacl_bn_mod_exp_ct(nLen, nh, ah, nBits, bh, 4, r2, ch);
+  //bn_compare_and_print(nBytes, c, ch);
+
+
+  for (int j = 0; j < ROUNDS; j++) {
+    hacl_bn_mod_exp_ct(nLen, nh, ah, nBits, bh, 4, r2, ch);
+  }
+
+  t1 = clock();
+  c1 = cpucycles_begin();
+  for (int j = 0; j < ROUNDS; j++) {
+    hacl_bn_mod_exp_ct(nLen, nh, ah, nBits, bh, 4, r2, ch);
+  }
+  c2 = cpucycles_end();
+  t2 = clock();
+  double diff1 = t2 - t1;
+  uint64_t cyc1 = c2 - c1;
+
+
+  for (int j = 0; j < ROUNDS; j++) {
+    ossl_bn_mod_exp_ct(a, b, n, c, ctx);
+  }
+
+  t1 = clock();
+  c1 = cpucycles_begin();
+  for (int j = 0; j < ROUNDS; j++) {
+    ossl_bn_mod_exp_ct(a, b, n, c, ctx);
+  }
+  c2 = cpucycles_end();
+  t2 = clock();
+  double diff2 = t2 - t1;
+  uint64_t cyc2 = c2 - c1;
+
+
+  uint64_t count = ROUNDS * SIZE;
+  //printf("\nBits = %d", nBits);
+  //printf("\nHACL* BN_mod_exp_mont_consttime\n"); print_time(count,diff1,cyc1);
+  //printf("\nOpenSSL BN_mod_exp_mont_consttime\n"); print_time(count,diff2,cyc2);
+  //printf("\nratio signature hacl/openssl %8.2f\n", (double)cyc1/cyc2);
+  // (double)cdiff/count
+  printf ("%d\t || %.2f\t || %.2f\t || %.2f \n", nBits, (double)cyc1/count, (double)cyc2/count, (double)cyc1/cyc2);
+
+}
+
+
+int main() {
+  printf ("nBits\t || hacl\t || openssl\t || hacl / openssl \n");
+
+  //for (int i = 256; i <= 8192; i = i * 2)
+  //test(i);
+  test(256);
+  test(512);
+  test(1024);
+  test(1536);
+  test(2048);
+  test(3072);
+  test(4096);
+  test(6144);
+  test(8192);
+
+  return EXIT_SUCCESS;
+}
